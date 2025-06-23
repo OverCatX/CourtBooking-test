42f13cb9564c1a2a72f09b16e02723f2f6b39cab
Bhumipat Kusalatham <overcatpocket@gmail.com>
2025-06-23 17:09:34 +0700

    Update booking page

--- client/src/app/booking/page.tsx
+++ client/src/app/booking/page.tsx
@@ -1,4 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import liff from "@line/liff";
+
 export default function Booking() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    liff.init({ liffId: "2007624537-O6KnVQar" }).then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login(); // login แล้วจะ reload กลับมาหน้านี้
+      } else {
+        liff.getProfile().then((profile) => {
+          setName(profile.displayName);
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="p-4 border border-gray-400 w-[500px] h-[500px]">

afa2d4c78906f9dffd316ee446cf387f8036fa5d
Bhumipat Kusalatham <overcatpocket@gmail.com>
2025-06-23 17:11:08 +0700

    Update booking page

--- client/src/app/booking/page.tsx
+++ client/src/app/booking/page.tsx
@@ -22,7 +22,7 @@ export default function Booking() {
     <div className="flex justify-center items-center h-screen">
       <div className="p-4 border border-gray-400 w-[500px] h-[500px]">
         <h1 className="text-black font-bold text-center text-3xl">Booking</h1>
-        <p className="font-bold text-center text-1xl">Hello: {}</p>
+        <p className="font-bold text-center text-1xl">Hello: {name}</p>
       </div>
     </div>
   );

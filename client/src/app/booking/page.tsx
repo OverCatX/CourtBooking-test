886229762df5983a218e36f12024cfd1d04e42a9
Bhumipat Kusalatham <overcatpocket@gmail.com>
2025-06-23 16:53:29 +0700

    added

---
+++ client/src/app/booking/page.tsx
@@ -0,0 +1,7 @@
+export default function Booking() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="p-4 border border-gray-400 w-[500px] h-[500px]"></div>
+    </div>
+  );
+}
